a9e18ba00b1a597b9de44cdd0ae25b8b28b04361
super <super1wzq1@outlook.com>
2026-02-02 00:47:11 +0800

    get instrcution

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,5 +1,11 @@
-import { ActionGetResponse, ACTIONS_CORS_HEADERS } from '@solana/actions'
+import { ActionGetResponse, ActionPostRequest, ACTIONS_CORS_HEADERS, createPostResponse } from '@solana/actions'
+import { Connection, PublicKey, Transaction, SystemProgram } from '@solana/web3.js';
+import { BN, Program } from '@coral-xyz/anchor';
+import { Voting } from '../../../../anchor/target/types/voting';
+import { connection } from 'next/server';
 
+
+const IDL = require('../../../../anchor/target/idl/voting.json');
 export const OPTIONS = GET; // 允许预检请求
 
 export async function GET(request: Request) {
@@ -13,15 +19,61 @@ export async function GET(request: Request) {
                 {
                     type: "transaction",
                     label: "Vote for Crunchy",
-                    href: "/api/vote?candidate=crunchy",
+                    href: "/api/vote?candidate=Crunchy",
                 },
                 {
                     type: "transaction",
                     label: "Vote for Smooth",
-                    href: "/api/vote?candidate=smooth",
+                    href: "/api/vote?candidate=Smooth",
                 }
             ]
         }
     }; 
     return Response.json(actionMetadata, { headers: ACTIONS_CORS_HEADERS}); // ACTIONS_CORS_HEADERS 允许跨域资源共享
 } 
+
+export async function POST(request: Request) {
+    
+    const url = new URL(request.url);
+    const candidate = url.searchParams.get("candidate");
+
+    if (candidate != "Crunchy" && candidate != "Smooth") {
+        return new Response("Invalid candidate", { status: 400, headers: ACTIONS_CORS_HEADERS});
+    }
+    const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+    const program: Program<Voting> = new Program(IDL, {connection}) 
+    const body: ActionPostRequest = await request.json();
+    let voter;
+
+    try {
+        voter = new PublicKey(body.account);
+    } catch (error) {
+        return new Response("Invalid voter account", { status: 400, headers: ACTIONS_CORS_HEADERS});
+    }
+    // instruction()生成一个 指令对象 (TransactionInstruction)，但是先别发出去
+    const instruction = await program.methods
+                                            .vote(new BN(1), candidate)
+                                            .accounts({
+                                                signer: voter,
+                                            })
+                                            .instruction();
+    const blockhash = await connection.getLatestBlockhash();
+    const transaction = new Transaction(
+        {
+            feePayer: voter,
+            blockhash: blockhash.blockhash,
+            lastValidBlockHeight: blockhash.lastValidBlockHeight,
+        }
+    ).add(instruction);
+
+    const response = await createPostResponse(
+        {
+            fields: {
+                type: "transaction",
+                transaction: transaction,
+            }
+        }
+    );
+
+    return Response.json(response, { headers: ACTIONS_CORS_HEADERS});
+}
